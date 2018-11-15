8c32568b7d9c4fe961b0a626907d3f85c9cfb31d
Assaf Eli <ASSAFEL@amdocs.com>
2018-11-15 17:48:04 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
+const cors = require("cors");
+
+const app = express();
+
+//use cross-origin
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+  console.log(`server started on ${PORT}`);
+});
